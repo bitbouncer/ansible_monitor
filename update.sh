bda2c7dff36bcfecfa34afc198087931ef81aec8
svante <svante.karlsson@csi.se>
2019-05-14 19:13:02 +0200

    add unzip and fix some default values

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 source ./config
 git pull
-ansible-playbook -i "localhost," --extra-vars "API_KEY=$API_KEY ALERT_RULES_URL=$ALERT_RULES_URL" -c local monitor.yml
+ansible-playbook -i "localhost," --extra-vars "bb_api_key=$API_KEY alert_rules_url=$ALERT_RULES_URL" -c local monitor.yml
 
 
