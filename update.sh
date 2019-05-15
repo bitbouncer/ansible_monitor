77e8acfc2415853508620b8fc693e587ddf6e462
svante <svante.karlsson@csi.se>
2019-05-15 10:07:13 +0200

    adds alertconfig from private github/bitbucket repos (as .zip archive)

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 source ./config
 git pull
-ansible-playbook -i "localhost," --extra-vars "bb_api_key=$API_KEY alertconfig_url=$ALERT_RULES_URL" -c local monitor.yml
+ansible-playbook -i "localhost," --extra-vars "bb_api_key=$API_KEY alertconfig_url=$ALERTCONFIG_URL alertconfig_user=$ALERTCONFIG_USER alertconfig_password=$ALERTCONFIG_PASSWORD" -c local monitor.yml
 
 
