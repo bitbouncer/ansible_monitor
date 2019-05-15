0615cf491bbd08fbda94f6ffbff829e4e8576e9b
svante <svante.karlsson@csi.se>
2019-05-15 05:55:53 +0200

    initial support for alertmanager

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 source ./config
 git pull
-ansible-playbook -i "localhost," --extra-vars "bb_api_key=$API_KEY alert_rules_url=$ALERT_RULES_URL" -c local monitor.yml
+ansible-playbook -i "localhost," --extra-vars "bb_api_key=$API_KEY alertconfig_url=$ALERT_RULES_URL" -c local monitor.yml
 
 
