183145cb465f8f52bf77c0efcef049b079bfa480
svante <svante.karlsson@csi.se>
2019-09-16 16:59:26 +0200

    update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 source ./config
 git pull
-ansible-playbook -i "localhost," --extra-vars "bb_api_key=$API_KEY alertconfig_url=$ALERTCONFIG_URL alertconfig_user=$ALERTCONFIG_USER alertconfig_password=$ALERTCONFIG_PASSWORD" -c local monitor.yml
+ansible-playbook -i "localhost," --extra-vars "monitor_read_api_key=$API_KEY monitor_read_secret_api_key=$SECRET_ACCESS_KEY alertconfig_url=$ALERTCONFIG_URL alertconfig_user=$ALERTCONFIG_USER alertconfig_password=$ALERTCONFIG_PASSWORD" -c local monitor.yml
 
 
